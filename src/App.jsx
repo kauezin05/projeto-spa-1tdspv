5aa64d7291dfb28f35e99db3234f44b54d465f7b
kauezin05 <kauezinho_naruto@hotmail.com>
2023-08-29 22:59:02 -0300

    Criando Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-import reactLogo from "./assets/react.svg";
 import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import reactLogo from "./assets/react.svg"
+import Rodape from "./components/Rodape";
+import {BrowserRouter,  Route, Routes } from "react-router-dom"
+
 
 export default function App() {
   
   //ÁREA DECLARATIVA
 
-  let reactLogoTextoAlt = "Logo do React";
+  let reactLogoTextAlt = "Logo React"
+
 
   return (
-    <>
-      {/* Área imperativa! */}
-      <div>
-        {/* Um cabeçalho com um header, um h1, uma lista ul com 3 itens com 3 links. */}
-
-        <Cabecalho/>
-
-        {/* Uma seção com uma div com 3 parágrafos, com 3 linhas de lorem, uma imagem */}
-        <section>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit accusantium repudiandae enim delectus deserunt itaque necessitatibus, obcaecati fugiat suscipit quasi error animi impedit esse distinctio! Cum impedit modi tempora laborum?</p>
-            <p>Incidunt quaerat id neque non dolores atque architecto, praesentium rerum, veritatis magnam iure rem commodi itaque illo vel modi numquam libero quam labore exercitationem laboriosam fuga est. Nam, nostrum commodi!</p>
-            <p>Velit tempora, hic facilis suscipit, beatae aliquid saepe harum aspernatur deserunt unde dicta blanditiis quod maiores accusantium sunt, itaque odit. Facilis magnam eius sapiente illo reiciendis quisquam saepe nisi at?</p>
-            <img src={reactLogo} alt={reactLogoTextoAlt} />
-          </div>
-        </section>
-        
-        {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociais */}
-        <footer>
-          <div>
-            <ul>
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">X</a></li>
-              <li><a href="#">Reddit</a></li>
-            </ul>
-          </div>
-        </footer>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx';
+import Produtos from './components/produtos/Produtos.jsx';
+import Error from './components/Error.jsx';
+import EditarProdutos from './components/produtos/EditarProduto.jsx';
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, children:[
+    {path: "/Home", element: <Home/>}
+    {path: "/produtos", element: <Produtos/>}
+    {path: "/produtos/editar/:id", element: <EditarProdutos/>}
+  ]}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
