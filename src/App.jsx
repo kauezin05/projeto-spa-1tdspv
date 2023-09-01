9686067125875a3df25acc07bc01cd500da5001b
Kaue <kauezinho_naruto@hotmail.com>
2023-08-31 21:50:24 -0300

    Arrumando erros e Add passo a passo no README

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
+import reactLogo from "./assets/react.svg";
 import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
-import reactLogo from "./assets/react.svg"
 import Rodape from "./components/Rodape";
-import {BrowserRouter,  Route, Routes } from "react-router-dom"
-
 
 export default function App() {
   
   //ÁREA DECLARATIVA
 
-  let reactLogoTextAlt = "Logo React"
-
+  let reactLogoTextoAlt = "Logo do React";
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" index element></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      {/* Área imperativa! */}
+      <div>
+        {/* Um cabeçalho com um header, um h1, uma lista ul com 3 itens com 3 links. */}
+      <Cabecalho/>
+        {/* Uma seção com uma div com 3 parágrafos, com 3 linhas de lorem, uma imagem */}
+
+      <Conteudo reactLogoProps={reactLogo} reactLogoTextoAltProps={reactLogoTextoAlt} />
+        
+        {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociais */}
+        <Rodape/>
+
+      </div>
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,18 @@ import Error from './components/Error.jsx';
 import EditarProdutos from './components/produtos/EditarProduto.jsx';
 
 const router = createBrowserRouter([
-  {path:"/", element:<App/>, children:[
-    {path: "/Home", element: <Home/>}
-    {path: "/produtos", element: <Produtos/>}
-    {path: "/produtos/editar/:id", element: <EditarProdutos/>}
-  ]}
+  {path: "/" , element: <App/>, errorElement: <Error/>,
+    children:[
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/produtos/editar/:id" , element: <EditarProdutos/>},
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
+
