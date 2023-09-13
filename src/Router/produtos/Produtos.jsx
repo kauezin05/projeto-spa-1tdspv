15bf90e4507e204cd98e2c292ca851b6805e057e
kauezin05 <kauezinho_naruto@hotmail.com>
2023-09-12 22:06:23 -0300

    Apresentando o CSS.MODULE

--- src/Router/produtos/Produtos.jsx
+++ src/Router/produtos/Produtos.jsx
@@ -1,47 +1,46 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "./ListaProdutos";
 import {AiFillEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
+import classes from "./Produtos.module.css"; 
 
 export default function Produtos() {
 
-    document.title = "Lista de Produtos";
+  document.title = "Lista de Produtos";
 
-    return (
+  return (
+    <div>
+        <h1>LISTA DE PRODUTOS</h1>
+
       <div>
-          <h1>LISTA DE PRODUTOS</h1>
-
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th>Preço</th>
-                <th>Editar/Excluir</th>
-                </tr>
-            </thead>
-            <tbody>
-              {ListaProdutos.map((produto, index) => (
-                <tr key={index}>
-                  <td>{produto.id}</td>
-                  <td>{produto.nome}</td>
-                  <td>{produto.desc}</td>
-                  <td>{produto.preco}</td>
-                  <td><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
-                </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan="4">Total de Produtos: {ListaProdutos.length}</td>
+        <table className={classes.tableStyle}>
+          <thead>
+            <tr className={classes.tableHeaderStyle}>
+              <th className={classes.tableHeaderStyle}>ID</th>
+              <th className={classes.tableHeaderStyle}>Nome</th>
+              <th className={classes.tableHeaderStyle}>Descrição</th>
+              <th className={classes.tableHeaderStyle}>Preço</th>
+              <th className={classes.tableHeaderStyle}>Editar/Excluir</th>
               </tr>
-            </tfoot>
-          </table>
-        </div>
-
+          </thead>
+          <tbody>
+            {ListaProdutos.map((produto, index) => (
+              <tr key={index} className={classes.tableLineStyle}>
+                <td className={classes.tableDataStyle}>{produto.id}</td>
+                <td className={classes.tableDataStyle}>{produto.nome}</td>
+                <td className={classes.tableDataStyle}>{produto.desc}</td>
+                <td className={classes.tableDataStyle}>{produto.preco}</td>
+                <td className={classes.tableDataStyle}><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
+              </tr>
+            ))} 
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="4">Total de Produtos: {ListaProdutos.length}</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-    )
-  }
-  
-  
+
+    </div>
+  )
+}
