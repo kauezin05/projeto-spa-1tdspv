0817711a4749a4cbd442f2763365edf843bdbf17
Kaue <kauezinho_naruto@hotmail.com>
2023-09-28 23:00:41 -0300

    Estilizando ModalInserir e Ultilizando o SCSS

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Cabecalho() {
   return (
-    <header>
-      <h1>Vite + React / RM551812</h1>
-      <ul>
-        <li><Link to="/">HOME</Link></li>
-        <li><Link to="/produtos">PRODUTOS</Link></li>
-      </ul>
-    </header>
+    <>
+      <header>
+        <h1>Vite + React / Coded By- PF0670</h1>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/produtos">PRODUTOS</Link>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 }
