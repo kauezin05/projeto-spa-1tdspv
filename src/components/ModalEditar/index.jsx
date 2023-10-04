3cf9ff001f1d6bfb7d2787dd03b54c635034ade0
Duh0127 <rodatupmocgamer22@gmail.com>
2023-10-03 23:23:13 -0300

    input de nome do produto

--- src/components/ModalEditar/index.jsx
+++ src/components/ModalEditar/index.jsx
@@ -16,6 +16,16 @@ export default function ModalEditar({ open, setOpen }) {
                         <fieldset className='fieldset'>
                             <button onClick={() => setOpen(false)}>X</button>
                             <legend>Cadastrar Produto</legend>
+                            <div>
+                                <label htmlFor="idProd">Nome do Produto</label>
+                                <input
+                                    type="text"
+                                    name="nome"
+                                    id="idProd"
+                                    onChange={handleChange}
+                                    value={produto.nome}
+                                />
+                            </div>
                             
                         </fieldset>
                     </form>
